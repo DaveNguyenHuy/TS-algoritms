650d1d81da845b9924a3fb56a14d1f5dc7e90318
davenguyenhuy <nguyenhuycuong.uet@gmail.com>
2021-09-26 10:16:07 +0700

    init double linked list , prettier

---
+++ linked_list/linked_list.test.ts
@@ -0,0 +1,162 @@
+import { LinkedList } from './linked_list';
+import exp = require('constants');
+
+test('Linked List =>', () => {
+  const myLinkedList = new LinkedList<number>(1);
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 1,
+      next: null,
+    },
+    tail: {
+      value: 1,
+      next: null,
+    },
+    length: 1,
+  });
+
+  myLinkedList.append(3); // 1 -> 3
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 1,
+      next: { value: 3, next: null },
+    },
+    tail: {
+      value: 3,
+      next: null,
+    },
+    length: 2,
+  });
+
+  myLinkedList.prepend(0); // 0 -> 1 -> 3
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 0,
+      next: { value: 1, next: { value: 3, next: null } },
+    },
+    tail: {
+      value: 3,
+      next: null,
+    },
+    length: 3,
+  });
+
+  myLinkedList.insert(2, 2); // 0 -> 1 -> 2 -> 3
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 0,
+      next: { value: 1, next: { value: 2, next: { value: 3, next: null } } },
+    },
+    tail: {
+      value: 3,
+      next: null,
+    },
+    length: 4,
+  });
+
+  myLinkedList.insert(4, 4); // 0 -> 1 -> 2 -> 3 -> 4
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 0,
+      next: {
+        value: 1,
+        next: { value: 2, next: { value: 3, next: { value: 4, next: null } } },
+      },
+    },
+    tail: {
+      value: 4,
+      next: null,
+    },
+    length: 5,
+  });
+
+  myLinkedList.insert(10, 5); // 0 -> 1 -> 2 -> 3 -> 4 -> 5
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 0,
+      next: {
+        value: 1,
+        next: {
+          value: 2,
+          next: {
+            value: 3,
+            next: { value: 4, next: { value: 5, next: null } },
+          },
+        },
+      },
+    },
+    tail: {
+      value: 5,
+      next: null,
+    },
+    length: 6,
+  });
+
+  myLinkedList.remove(0); // 1 -> 2 -> 3 -> 4 -> 5
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 1,
+      next: {
+        value: 2,
+        next: { value: 3, next: { value: 4, next: { value: 5, next: null } } },
+      },
+    },
+    tail: {
+      value: 5,
+      next: null,
+    },
+    length: 5,
+  });
+
+  myLinkedList.remove(5); // 1 -> 2 -> 3 -> 4
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 1,
+      next: { value: 2, next: { value: 3, next: { value: 4, next: null } } },
+    },
+    tail: {
+      value: 4,
+      next: null,
+    },
+    length: 4,
+  });
+
+  myLinkedList.remove(2); // 1 -> 2 -> 4
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 1,
+      next: { value: 2, next: { value: 4, next: null } },
+    },
+    tail: {
+      value: 4,
+      next: null,
+    },
+    length: 3,
+  });
+
+  myLinkedList.remove(2); // 1 -> 2
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 1,
+      next: { value: 2, next: null },
+    },
+    tail: {
+      value: 2,
+      next: null,
+    },
+    length: 2,
+  });
+
+  myLinkedList.remove(1); // 1
+  expect(myLinkedList).toEqual({
+    head: {
+      value: 1,
+      next: null,
+    },
+    tail: {
+      value: 1,
+      next: null,
+    },
+    length: 1,
+  });
+});
--- linked_list/linked_list.ts
+++ linked_list/linked_list.ts
@@ -13,65 +13,64 @@ export class LinkedList<T> {
   private head: INode<T>;
   private tail: INode<T>;
   length: number = 0;
-  
+
   constructor(value: T) {
-    const node = new INode<T>(value)
+    const node = new INode<T>(value);
     this.head = node;
     this.length = 1;
-    this.tail = node
+    this.tail = node;
   }
-  
+
   append(value: T) {
     const newNode = new INode<T>(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
   }
-  
+
   prepend(value: T) {
-    const newNode = new INode<T>(value)
+    const newNode = new INode<T>(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
   }
-  
+
   insert(index: number, value: T) {
     if (index <= 0) {
-      return this.prepend(value)
+      return this.prepend(value);
     }
     if (index >= this.length) {
-      return this.append(value)
+      return this.append(value);
     }
     const newNode = new INode<T>(value);
-    let node: INode<T> = this.traverseToIndex(index - 1)
+    let node: INode<T> = this.traverseToIndex(index - 1);
     newNode.next = node.next;
     node.next = newNode;
-    this.length++
+    this.length++;
   }
-  
+
   remove(index: number) {
     if (index <= 0) {
       this.head = this.head.next;
     } else if (index >= this.length - 1) {
-      const last = this.traverseToIndex(this.length - 2)
-      last.next = null
+      const last = this.traverseToIndex(this.length - 2);
+      last.next = null;
       this.tail = last;
     } else {
-      const prevNode = this.traverseToIndex(index - 1)
-      const unwantedNode = prevNode.next
+      const prevNode = this.traverseToIndex(index - 1);
+      const unwantedNode = prevNode.next;
       prevNode.next = unwantedNode.next;
     }
-    this.length--
+    this.length--;
   }
-  
+
   private traverseToIndex(index: number) {
     let current = 0;
     let node: INode<T> = this.head;
     while (current < index) {
       node = node.next;
-      current++
+      current++;
     }
-    return node
+    return node;
   }
 }
-
