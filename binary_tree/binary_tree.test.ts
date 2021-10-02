390786412898280aa4c311913d39a992c9b902be
davenguyenhuy <nguyenhuycuong.uet@gmail.com>
2021-10-02 16:41:02 +0700

    binary tree insert and lookup

---
+++ binary_tree/binary_tree.test.ts
@@ -0,0 +1,84 @@
+import { BinaryTree } from './binary_tree';
+import exp = require('constants');
+
+describe('Binary Tree', () => {
+  let tree: BinaryTree<number>;
+
+  beforeEach(() => {
+    tree = new BinaryTree<number>();
+  });
+
+  afterEach(() => {
+    tree = null;
+  });
+
+  test('insert value to empty tree', () => {
+    tree.insert(0);
+    expect(tree).toEqual({
+      root: {
+        value: 0,
+        right: null,
+        left: null,
+      },
+    });
+  });
+
+  test('insert multiple nodes', () => {
+    tree.insert(0);
+    tree.insert(1);
+    tree.insert(2);
+
+    expect(tree).toEqual({
+      root: {
+        value: 0,
+        left: null,
+        right: {
+          value: 1,
+          left: null,
+          right: {
+            value: 2,
+            right: null,
+            left: null,
+          },
+        },
+      },
+    });
+  });
+
+  test('lookup in binary tree', () => {
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(20);
+    tree.insert(4);
+
+    expect(tree).toEqual({
+      root: {
+        value: 10,
+        left: {
+          value: 5,
+          right: null,
+          left: { value: 4, right: null, left: null },
+        },
+        right: {
+          value: 20,
+          right: null,
+          left: null,
+        },
+      },
+    });
+
+    expect(tree.lookup(5)).toEqual({
+      value: 5,
+      right: null,
+      left: { value: 4, right: null, left: null },
+    });
+
+    expect(tree.lookup(4)).toEqual({
+      value: 4,
+      right: null,
+      left: null,
+    });
+
+    expect(tree.lookup(100)).toBeNull();
+  });
+});
---
+++ binary_tree/binary_tree.ts
@@ -0,0 +1,45 @@
+class BinaryTreeNode<T> {
+  value: T;
+  right: null | BinaryTreeNode<T>;
+  left: null | BinaryTreeNode<T>;
+
+  constructor(value: T, right = null, left = null) {
+    this.value = value;
+    this.right = right;
+    this.left = left;
+  }
+}
+
+export class BinaryTree<T> {
+  root: null | BinaryTreeNode<T>;
+
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value: T) {
+    const node = new BinaryTreeNode<T>(value);
+    if (!this.root) {
+      return (this.root = node);
+    }
+    let pointer: null | BinaryTreeNode<T> = this.root;
+    while (true) {
+      if (pointer.value < value) {
+        if (pointer.right) pointer = pointer.right;
+        else return (pointer.right = new BinaryTreeNode<T>(value));
+      } else {
+        if (pointer.left) pointer = pointer.left;
+        else return (pointer.left = new BinaryTreeNode<T>(value));
+      }
+    }
+  }
+
+  lookup(value: T) {
+    let pointer = this.root;
+    while (pointer) {
+      if (pointer?.value === value) return pointer;
+      pointer = pointer?.value < value ? pointer.right : pointer.left;
+    }
+    return pointer;
+  }
+}
